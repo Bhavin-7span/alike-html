7bf066498203b4666282c7e0da461fbb051a5919
Komal Gajjar <komalgajjar@Komal-Mac-mini.local>
2022-06-22 20:05:46 +0530

    WIP City detail page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,15 +45,54 @@ $(document).ready(function() {
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 1.5,
+                    slidesToShow: 2,
                 }
             },
             {
                 breakpoint: 475,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 1.5,
                 }
             }
         ],
     });
+
+    $('.cities-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 7000,
+        cssEase: "linear",
+        pauseOnHover: true,
+        arrows: false,
+        dots: false,
+        responsive: [{
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3.5,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2.5,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 475,
+                settings: {
+                    slidesToShow: 1.5,
+                }
+            }
+        ],
+    });
+
 });
