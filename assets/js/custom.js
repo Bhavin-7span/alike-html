f169926ca2ff9212e041014aaba495d5037300a4
Komal Gajjar <komalgajjar@Komal-Mac-mini.local>
2022-06-24 19:30:16 +0530

    WIP Detail page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -109,8 +109,8 @@ $(document).ready(function () {
     // cssEase: "linear",
     // pauseOnHover: true,  
     arrows: true,
-    prevArrow: '<button type="button" class="slick-prev slider--arrow"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.9998 19.9201L8.47984 13.4001C7.70984 12.6301 7.70984 11.3701 8.47984 10.6001L14.9998 4.08008" stroke="#D4CDD4" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>',
-    nextArrow: '<button type="button" class="slick-next slider--arrow"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008" stroke="#D4CDD4" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>',
+    prevArrow: '<button type="button" class="slick-prev slider--arrow"><i class="bi bi-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next slider--arrow"><i class="bi bi-chevron-right"></i></button>',
     dots: false,
     responsive: [
       {
