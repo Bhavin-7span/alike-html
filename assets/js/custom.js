1644cd9efb8d528d2947801e9cb7c11032e9a572
Komal Gajjar <komalgajjar@Komal-Mac-mini.local>
2022-06-24 18:44:52 +0530

    WIP detail page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -38,22 +38,24 @@ $(document).ready(function () {
     ],
   });
 
-  $(".cities-wrapper").slick({
+  $(".cities-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 0,
-    speed: 7000,
-    cssEase: "linear",
-    pauseOnHover: true,
-    arrows: false,
+    speed: 5000,
+    // cssEase: "linear",
+    // pauseOnHover: true,  
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev slider--arrow"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.9998 19.9201L8.47984 13.4001C7.70984 12.6301 7.70984 11.3701 8.47984 10.6001L14.9998 4.08008" stroke="#D4CDD4" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>',
+    nextArrow: '<button type="button" class="slick-next slider--arrow"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.91016 19.9201L15.4302 13.4001C16.2002 12.6301 16.2002 11.3701 15.4302 10.6001L8.91016 4.08008" stroke="#D4CDD4" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>',
     dots: false,
     responsive: [
       {
         breakpoint: 1300,
         settings: {
           slidesToShow: 3.5,
-          infinite: true,
+          // infinite: true,
         },
       },
       {
