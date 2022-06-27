020538071298c646f3f51b629f494b4525f7a9a4
jenil-7span <jenil@7span.com>
2022-06-27 19:34:42 +0530

    Changes resolved

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,11 +57,131 @@ $(document).ready(function () {
           arrows: false,
           dots: false,
           focusOnSelect: true,
+          responsive: [
+            {
+              breakpoint: 1280,
+              settings:{
+                slidesToShow: 10
+              }
+            }
+          ]
         });
       },
       function () {}
     );
   });
+
+  $('.show__more').on('click', function() {
+    let _this = $(this);
+    setTimeout(function(){
+      let element = `<div class="col-6 col-md-3 mt-4">
+      <div class="common-card">
+                  <a href="javascript:;" class="d-block common-card__media position-relative">
+                    <div class="place-overlay position-absolute top-0 start-0 end-0 bottom-0 w-100 h-100">
+                    </div>
+                    <img class="common-card__image" src="assets/img/dubai.png" alt="dubai">
+                    <p class="common-card__title">
+                      Dubai</p>
+                  </a>
+                </div>
+                <ul class="mt-2 ps-4 d-none d-md-block">
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Dubai Burj Khalifa
+                      Tour</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">SIM Cards</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Global Village</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">72 Hours of Fun</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3 mt-4">
+                <div class="common-card">
+                  <a href="javascript:;" class="d-block common-card__media position-relative">
+                    <div class="place-overlay position-absolute top-0 start-0 end-0 bottom-0 w-100 h-100">
+                    </div>
+                    <img class="common-card__image" src="assets/img/abu-dhabi.png" alt="abu-dhabi">
+                    <p class="common-card__title">
+                      Abu Dhabi</p>
+                  </a>
+                </div>
+                <ul class="mt-2 ps-4 d-none d-md-block">
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Al Ain City Tour</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Falcon Tour Abu Dhabi
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Sunrise Desert
+                      Safari</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">4
+                      Day Family Trip</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3 mt-4">
+                <div class="common-card">
+                  <a href="javascript:;" class="d-block common-card__media position-relative">
+                    <div class="place-overlay position-absolute top-0 start-0 end-0 bottom-0 w-100 h-100">
+                    </div>
+                    <img class="common-card__image" src="assets/img/phuket.png" alt="phuket">
+                    <p class="common-card__title">
+                      Phuket</p>
+                  </a>
+                </div>
+                <ul class="mt-2 ps-4 d-none d-md-block">
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Things to do</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">SIM Cards</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Burj Khalifa</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Weekend Getaway</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3 mt-4">
+                <div class="common-card">
+                  <a href="javascript:;" class="d-block common-card__media position-relative">
+                    <div class="place-overlay position-absolute top-0 start-0 end-0 bottom-0 w-100 h-100">
+                    </div>
+                    <img class="common-card__image" src="assets/img/bangkok.png" alt="bangkok">
+                    <p class="common-card__title">
+                      Bangkok</p>
+                  </a>
+                </div>
+                <ul class="mt-2 ps-4 d-none d-md-block">
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Things to do</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">SIM Cards</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Burj Khalifa</a>
+                  </li>
+                  <li>
+                    <a class="dark-gray fs-6 fw-normal d-block asia-link mt-1" href="javascript:;">Weekend Getaway</a>
+                  </li>
+                </ul>
+                      </div>`
+      _this.parents('.popular-list').append(element);
+      _this.parents('.popular-list .btn-card').hide();
+    },1000)
+  })
 
   $(".places-slider").slick({
     slidesToShow: 4,
@@ -95,8 +215,10 @@ $(document).ready(function () {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
+          dots: true,
+          arrows: false,
         },
-      },
+      }
     ],
   });
 
